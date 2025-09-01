84b83575733e69ec2269cf3f652046937bcbfaa7
dejediah <raphaelborillo@gmail.com>
2025-09-01 18:26:25 +0800

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/beginning';
 import Runes from './pages/runes.js'; 
 import Contract from './pages/contract.js'; 
